35028fb2b53b9cd76cf39bd27de545bd7e485edc
randirao <randirao4245@gmail.com>
2025-12-03 00:51:17 +0900

    feat: Content Security Policy 미들웨어 추가

---
+++ src/middlewares/csp.js
@@ -0,0 +1,19 @@
+export function cspMiddleware(req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; " +
+      "script-src 'self' 'unsafe-inline'; " +
+      "style-src 'self' 'unsafe-inline'; " +
+      "img-src 'self' data: https:; " +
+      "font-src 'self'; " +
+      "connect-src 'self'; " +
+      "frame-ancestors 'none';"
+  );
+
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+
+  next();
+}
