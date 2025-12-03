1883b86af1f24d113ce5a4a46667ca120c779c1b
randirao <randirao4245@gmail.com>
2025-12-03 11:00:48 +0900

    feat: Swagger UI 엔드포인트 통합

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
 import { config } from './config/index.js';
+import { swaggerSpec } from './config/swagger.js';
 import { initDatabase } from './models/db.js';
 import { cspMiddleware } from './middlewares/csp.js';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js';
@@ -13,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cspMiddleware);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/haram/auth', authRoutes);
 app.use('/tch', tchRoutes);
@@ -29,6 +33,7 @@ initDatabase();
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
   console.log(`Environment: ${config.nodeEnv}`);
+  console.log(`Swagger docs available at http://localhost:${config.port}/api-docs`);
 });
 
 export default app;
